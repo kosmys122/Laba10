24ee2fc5642d0e75e45ebeeb7c9ce3847ba085ce
Шапкин Николай <kosmys122@gmail.com>
2025-10-23 18:25:38 +0300

    Задание 1

---
+++ Ex1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new[]
+            {
+                new{Name="Рома",Age=5,Group="ИСП-231"},
+                new{Name="Алёна",Age=13,Group="ИСП-23113"},
+                new{Name="Дима",Age=51,Group="ЮСП-32"},
+                new{Name="Майкл",Age=18,Group="ЭСП-3"},
+                new{Name="Олег",Age=21,Group="ЗСП-9"}
+            };
+            foreach(var student in students)
+            {
+                Console.WriteLine($"{student.Name}, {student.Age}, {student.Group}.");
+            }
+        }
+    }
+}
