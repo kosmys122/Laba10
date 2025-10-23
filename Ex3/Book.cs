3b6bbcb0c57fee35924a7c93c14caf7f5d9fe41b
Шапкин Николай <kosmys122@gmail.com>
2025-10-23 19:21:12 +0300

    Задание 3

---
+++ Ex3/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex3
+{
+    internal class Book : IPrintable
+    {
+        public string Name { get; set; }
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Название книги {Name}");
+        }
+    }
+}
---
+++ Ex3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<IPrintable> items = new List<IPrintable>
+            {
+                new Book{Name="Война и мир"},
+                new Magazine{Name="Сметана"}
+            };
+            foreach(var item in items)
+            {
+                item.PrintInfo();
+            }
+        }
+    }
+}
