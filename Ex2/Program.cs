392e9b25788d479cdfaf90f8ab44c3da485d77ba
Шапкин Николай <kosmys122@gmail.com>
2025-10-23 19:08:21 +0300

    Задание 2

---
+++ Ex2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums1 = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+            List<int> nums2 = nums1.FindAll(x => x % 2 == 0);
+            List<int> nums3 = nums1.FindAll(x => x % 3 == 0);
+            Console.WriteLine(string.Join(", ", nums2));
+            Console.WriteLine(string.Join(", ", nums3));
+        }
+    }
+}
