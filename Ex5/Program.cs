6377ada6b73645d265099de02df93b57b08c69de
Шапкин Николай <kosmys122@gmail.com>
2025-10-23 19:48:41 +0300

    Задание 5

---
+++ Ex5/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Ex5
+{
+    internal class Program
+    {
+        static async Task<int> GetDataAsync(int a)
+        {
+            Console.WriteLine("Ждём данные…");
+            await Task.Delay(2000);
+            Console.WriteLine("Данные получены!");
+            return a;
+        }
+        static async Task Main()
+        {
+            await GetDataAsync(5);
+            await GetDataAsync(2);
+        }
+    }
+}
